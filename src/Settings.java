cf1001bf6da4004ea61012d16d40c5c281c968d0
s <s@10.15.134.43>
2016-11-08 21:31:37 -0800

    Added variables and getters/setters for settings

--- src/Settings.java
+++ src/Settings.java
@@ -1,11 +1,33 @@
+// Settings class for our game
 
+/*
+ * Settings will hold our settings for if the game sound is on or off as well as the music. 
+ * Volume will be up to the hardware of the device the user is playing on.
+ */
 public class Settings {
-//testing settings commit
+
+	private boolean isMusicOn;
+	private boolean isSoundOn;
 	
+	Settings(){
+		isMusicOn = true;
+		isSoundOn = true;
+	}
+
+	public boolean getIsMusicOn(){
+		return isMusicOn;
+	}
 	
+	public boolean getIsSoundOn(){
+		return isSoundOn;
+	}
 	
+	public void setIsSoundOn(boolean s){
+		isSoundOn = s;
+	}
 	
+	public void setIsMusicOn(boolean m){
+		isMusicOn = m;
+	}
 	
-	
-	//hkjhkj;
 }
