ec1083180cdea8fbfd45ae599ad481caea84045c
Maximo <m_macchi@u.pacific.edu>
2016-11-10 15:31:41 -0800

    Added member variables, getters, and setters to Bullet.java

--- src/game/Bullet.java
+++ src/game/Bullet.java
@@ -1,5 +1,32 @@
 package game;
+import acm.graphics.*;
 
 public class Bullet {
+	
+	private GImage sprite;
+	private Entity sender;
+	private Direction direction;
+	private static final int DAMAGE = 1;
+	private static final int SPEED = 1;
+	
+	private GImage getSprite() {
+		return sprite;
+	}
+	
+	private void setSprite(GImage s) {
+		sprite = s;
+	}
+	
+	private Entity getSender() {
+		return sender;
+	}
+	
+	private Direction getDirection() {
+		return direction;
+	}
+	
+	private void setDirection(Direction d) {
+		direction = d;
+	}
 	
 }
