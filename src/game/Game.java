cc8d26be421b4076316658706e233cf2ffd11172
jackthias <jackthias@icloud.com>
2016-11-10 13:48:33 -0800

    Removed todos.

--- src/game/Game.java
+++ src/game/Game.java
@@ -13,11 +13,9 @@ public class Game extends GraphicsPane {
 
 	public Game(MainApplication app) {
 		super(app);
-		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
