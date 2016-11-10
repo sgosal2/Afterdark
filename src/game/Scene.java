7aa63bf3c877ed6ee801ab093ca8f76c5cd22d6e
jackthias <jackthias@icloud.com>
2016-11-10 15:49:02 -0800

    Tiles are now drawn.

--- src/game/Scene.java
+++ src/game/Scene.java
@@ -1,13 +1,20 @@
 package game;
+
 import java.awt.Rectangle;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Scene {
 
+	public static int TILE_WIDTH;
+	public static int TILE_HEIGHT;
+	
 	private SceneLayout layout;
 	
-	public Scene() {
-		
+	public Scene(int tileWidth, int tileHeight) {
+		TILE_WIDTH = tileWidth;
+		TILE_HEIGHT = tileHeight;
+		layout = new SceneLayout(tileWidth, tileHeight);
 	}
 
 	public boolean checkTerrainCollisions(Entity player) {
@@ -18,5 +25,9 @@ public class Scene {
 		else
 			return true;
 	}
-
+	
+	public List<List<Block>> getTerrain() {
+		return layout.getTerrain();
+	}
+	
 }
