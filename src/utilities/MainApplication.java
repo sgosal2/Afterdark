322b1cc2bfcdbfea88203ee9640e12af0ab41b79
jackthias <jackthias@icloud.com>
2016-11-17 15:28:49 -0800

    Cleaned up MainApplication.

--- src/utilities/MainApplication.java
+++ src/utilities/MainApplication.java
@@ -4,8 +4,6 @@ package utilities;
  * This is my second commit.
  */
 
-import javax.swing.Timer;
-
 import game.Game;
 import menus.MenuPane;
 import menus.PauseMenu;
@@ -16,22 +14,17 @@ public class MainApplication extends GraphicsApplication {
 	public static final int WINDOW_WIDTH = 1024;
 	public static final int WINDOW_HEIGHT = 768;
 	
-	//private SomePane somePane;
 	private GraphicsPane menu;
 	private GraphicsPane game;
 	private GraphicsPane pauseMenu;
 	private GraphicsPane settings;
 	private GraphicsPane controls;
-	private int count = 0;
-	//private Timer gameLoop;
 	
 	public void init() {
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 	}
 	
 	public void run() {
-		//System.out.println("Hello, world!");
-		//somePane = new SomePane(this);
 		menu = new MenuPane(this);
 		game = new Game(this);
 		pauseMenu = new PauseMenu(this);
@@ -39,7 +32,6 @@ public class MainApplication extends GraphicsApplication {
 		controls = new Controls(this);
 		setupInteractions();
 		switchToMenu();
-		//gameLoop = new Timer(50, this);
 	}
 	
 	/* Method: setupInteractions
@@ -55,21 +47,10 @@ public class MainApplication extends GraphicsApplication {
 	}
 	
 	public void switchToMenu() {
-//		AudioPlayer audio = AudioPlayer.getInstance();
-//		switch(count % 2) {
-//			case 0: audio.stopSound("sounds", "r2d2.mp3"); break;
-//			case 1: audio.stopSound("sounds", "somethinlikethis.mp3"); break;
-//		}
-//		count++;
 		switchToScreen(menu);
 	}
 	
 	public void switchToGame() {
-//		AudioPlayer audio = AudioPlayer.getInstance();
-//		switch(count % 2) {
-//			case 0: audio.playSound("sounds", "r2d2.mp3"); break;
-//			case 1: audio.playSound("sounds", "somethinlikethis.mp3"); break;
-//		}
 		switchToScreen(game);
 	}
 	
